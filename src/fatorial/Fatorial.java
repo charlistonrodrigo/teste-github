d73429e4f394b473a53762dfa6955bee50fd0ae0
Charliston Rodrigo Cezar <charlistonrodrigo75@gmail.com>
2020-03-22 10:14:55 -0300

    Acrescentada mensagem de Bom Dia

--- src/fatorial/Fatorial.java
+++ src/fatorial/Fatorial.java
@@ -15,8 +15,8 @@ public class Fatorial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("AAA");
+       
+       
     }
     
 }
